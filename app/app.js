091fad29c79384ea675f39f38529a53638e8fc6f
李辉 <lihui@mucang.cn>
2017-06-11 11:51:46 +0800

    升级版本v1,添加远程更新服务的接口：/accumulates/v1/updateServer

--- app/app.js
+++ app/app.js
@@ -1,12 +1,21 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
+#V1:since 20170611
+function respondV1HelloWorld(req, res, next) {
   res.send('hello ， 周杰伦，我是朴树！this is a message from lihux, enjoy your life ^_^');
 }
 
-var server = restify.createServer();
-server.get('/hello/lihux', respond);
+function respondV1UpdateServer(req, res, next) {
+  res.send('服务端已经成功更新上线');
+}
 
-server.listen(8080, function() {
+#createServer
+var server = restify.createServer();
+var v1 = '/accumulates/v1'
+server.get(v1 + '/hello/lihux', respondV1HelloWorld);
+
+server.get(v1 + '/updateServer', respondV1UpdateServer);
+
+server.listen(1113, function() {
   console.log('listening at %s', server.url);
 });
