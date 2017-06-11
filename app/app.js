83d0df5bad9f91e0e1b4d9cb241a0419abe6468b
李辉 <lihui@mucang.cn>
2017-06-11 12:27:38 +0800

    app.js通过child_process（node.js）的方式调用拉取服务器脚本代码

--- app/app.js
+++ app/app.js
@@ -6,15 +6,27 @@ function respondV1HelloWorld(req, res, next) {
 }
 
 function respondV1UpdateServer(req, res, next) {
+	require("child_process").execFile('/home/ec2-user/restify/updateServer.sh', nil, (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+});
   res.send('服务端已经成功更新上线');
+}
+
+function respondV1HelloWorld(req, res, next) {
+  res.send('hello ， I love you!');
 }
 
 //createServer
 var server = restify.createServer();
 var v1 = '/accumulates/v1'
-server.get(v1 + '/hello/lihux', respondV1HelloWorld);
 
+server.get(v1 + '/hello/lihux', respondV1HelloWorld);
 server.get(v1 + '/updateServer', respondV1UpdateServer);
+//server.get(v1 + '/love', respondV1Love);
+
+
 
 server.listen(1113, function() {
   console.log('listening at %s', server.url);
